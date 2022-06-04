735fcdb56af1f5bd6b14208fbea50b635fd395ad
Emir Kutlugün <emirkutlugun01@gmail.com>
2022-06-04 21:04:35 +0300

    Initial Commit

---
+++ Chat ApplicationUITests/Chat_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chat_ApplicationUITestsLaunchTests.swift
+//  Chat ApplicationUITests
+//
+//  Created by Emir Kutlugün on 4.06.2022.
+//
+
+import XCTest
+
+class Chat_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
